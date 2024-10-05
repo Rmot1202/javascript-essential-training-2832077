7ef68f2bdf6d492e186862b58287456bd8eabd8d
Rmot1202 <Rmot1202@students.vsu.edu>
2024-10-05 00:25:29 -0400

    added by cmd

--- 03_06/script.js
+++ 03_06/script.js
@@ -22,3 +22,7 @@ const backpack = {
 };
 
 console.log("The backpack object:", backpack);
+console.log("The pocketNum value:", backpack.pocketNum);
+console.log("Strap length L:", backpack.strapLength.left);
+var query="pocketNum";
+console.log("The pocketNum value:", backpack[query]);
